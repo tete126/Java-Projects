54410dc8ba7ee3e7ade160256d6ba302e206578d
tete126 <tete.tao@gmail.com>
2024-09-21 10:12:28 -0400

    Create Main.java

---
+++ APCSHW16Q1/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
