ff0b929681a1be56a5651d7832d8a3610b8138b4
tete126 <tete.tao@gmail.com>
2024-09-21 12:45:53 -0400

    Create C4q2.java

---
+++ C4q2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class C4q2 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    for(int i = 1; i <= n; i++)
+      {
+        System.out.println(n + " X " + i + " = " + n * i);
+      }
+  }
+
+
+}
