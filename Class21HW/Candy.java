53f893b52a2bf55c025df9d2b1865ff3496f842e
tete126 <tete.tao@gmail.com>
2024-09-21 10:31:16 -0400

    Create Candy.java

---
+++ Class21HW/Candy.java
@@ -0,0 +1,14 @@
+public class Candy
+  {
+    private String flavor;
+    
+    public Candy(String flavor)
+    {
+      this.flavor = flavor;
+    }
+
+    public String getFlavor()
+    {
+      return flavor;
+    }
+  }
