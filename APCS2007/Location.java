24e04450148a3f5568ad65dd843f1300316b2445
tete126 <tete.tao@gmail.com>
2024-09-21 10:36:06 -0400

    Create Location.java

---
+++ APCS2007/Location.java
@@ -0,0 +1,22 @@
+public class Location
+  {
+    private int r;
+    private int c;
+
+    public Location(int r, int c)
+    {
+      this.r = r;
+      this.c = c;
+    }
+
+    public int getRow()
+    {
+      return r;
+    }
+
+    public int getCol()
+    {
+      return c;
+    }
+  
+  }
