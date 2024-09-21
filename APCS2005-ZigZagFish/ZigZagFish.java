e24929e750b572b311b1a94443eddcfea4453665
tete126 <tete.tao@gmail.com>
2024-09-20 22:07:52 -0400

    Create ZigZagFish.java

---
+++ APCS2005-ZigZagFish/ZigZagFish.java
@@ -0,0 +1,16 @@
+public class ZigzagFish extends Fish
+{
+  private boolean willZigRight;
+
+  public ZigZagFish (Environment env, Location loc)
+  {
+    super(env, loc);
+    willZigRight = true;
+  }
+
+  protected Location nextLocation()
+  {
+    
+  }
+  
+}
