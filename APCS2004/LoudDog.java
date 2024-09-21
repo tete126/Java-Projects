ad9f030a2e76767de804ff1e2b2ee70402d33e0c
tete126 <tete.tao@gmail.com>
2024-09-20 21:08:51 -0400

    Create LoudDog.java

---
+++ APCS2004/LoudDog.java
@@ -0,0 +1,12 @@
+public class LoudDog extends Dog
+{
+  public LoudDog(String name)
+  {
+    super(name);
+  }
+
+  public String speak()
+  {
+    return super.speak() + " " + super.speak();
+  }
+}
