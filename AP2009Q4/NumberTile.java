bca3bccdd89ed057001f52002c0b7b239a694b80
tete126 <tete.tao@gmail.com>
2024-09-21 14:06:03 -0400

    Create NumberTile.java

---
+++ AP2009Q4/NumberTile.java
@@ -0,0 +1,20 @@
+public class NumberTile
+{
+//Rotates the tile 90 degrees clockwise
+
+public void rotate ()
+{
+
+}
+//* implementation not shown 
+// @return value at left edge of tile
+public int getLeft ()
+{
+
+}
+//@return value at right edge of tile
+public int getRight ( )
+(
+  
+}
+}
