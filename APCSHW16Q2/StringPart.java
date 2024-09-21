fdcbef6f2369029ded300bf00260a2c99cac51e5
tete126 <tete.tao@gmail.com>
2024-09-21 10:15:29 -0400

    Create StringPart.java

---
+++ APCSHW16Q2/StringPart.java
@@ -0,0 +1,21 @@
+public class StringPart
+  {
+    private int start;
+    private int length;
+    
+    public StringPart(int start, int length)
+    {
+      this.start = start;
+      this.length = length;
+    }
+
+    public int getStart()
+    {
+      return start;
+    }
+
+    public int getLength()
+    {
+      return length;
+    }
+  }
